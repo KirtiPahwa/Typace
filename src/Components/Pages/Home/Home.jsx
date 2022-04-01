48589bc88a7e1929735871907027efb370b2aba2
Kirti <pahwakirti09942@gmail.com>
2022-04-01 15:26:15 +0530

    Boiler plate added

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="p-10 min-h-screen flex items-center justify-center bg-cool-gray-700">
+    <h1 className="text-9xl font-black text-white text-center">
+      <span className="bg-gradient-to-r text-transparent bg-clip-text from-green-400 to-purple-500">
+        Kirt
+      </span>
+    </h1>
+  </div>
+)
 }
